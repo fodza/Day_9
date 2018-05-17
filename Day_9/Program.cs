97afbf06ced643f42af6156112a13c034aec77c2
Agnese <fodza@inbox.lv>
2018-05-17 20:08:57 +0300

    Day_9
    Day-9

---
+++ Day_9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test TestaObjekts = new Test();
+            //Massina/ford = new Masina(); 
+            int rezultats = TestaObjekts.Saskaitisana(3, 7);
+
+            Test TestaObjekts2 = new Test();
+
+            TestaObjekts.skaitlis = 3;
+            TestaObjekts2.skaitlis = 6;
+            Console.WriteLine("Izvade no TestaObjekts: " + TestaObjekts.skaitlis);
+            Console.WriteLine("Izvade no TestaObjekts2: " + TestaObjekts2.skaitlis);
+
+
+            TestaObjekts.Izvade();
+            Console.WriteLine("Izvade no klases: " + rezultats);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Day_9/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    class Test
+    {
+        //public, private
+
+        public int skaitlis;
+
+        public int Saskaitisana(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void Izvade()
+        {
+            Console.WriteLine("test!");
+        }
+    }
+}
